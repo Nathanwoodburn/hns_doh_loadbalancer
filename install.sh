de7dd7fe9a43dd583a100b7cb594e22f65ae5879
Nathan Woodburn <github@nathan.woodburn.au>
2024-06-20 13:58:44 +1000

    feat: Update install instructions

--- install.sh
+++ install.sh
@@ -18,12 +18,43 @@ sudo systemctl restart systemd-resolved
 
 # Move the conf file to the correct location
 sudo cp ./dnsdist.conf /etc/dnsdist/dnsdist.conf
-# Replace the user and group in the dnsdist.service file to root
-# Like this
-# User=root
-# Group=root
 sudo cp ./dnsdist.service /lib/systemd/system/dnsdist.service
 sudo systemctl daemon-reload
 
 # Restart dnsdist
 sudo systemctl restart dnsdist
+
+
+# Install caddy
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy -y
+
+# Move the Caddyfile to the correct location
+sudo cp ./Caddyfile /etc/caddy/Caddyfile
+
+# Restart caddy
+sudo systemctl restart caddy
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+cd /root # Double check this path
+
+# Install Node.js
+nvm install 20.14.0
+npm install -g node-gyp
+
+# Install HSD
+git clone --depth 1 --branch latest https://github.com/handshake-org/hsd.git
+cd hsd
+npm install --omit=dev
+
+sudo cp ./hsd.service /lib/systemd/system/hsd.service
+sudo systemctl daemon-reload
+sudo systemctl enable hsd
+sudo systemctl start hsd
---
+++ test_inf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Get Node IPs
+RESOLVED_IPS=$(dig +short hnsdoh.com)
+NODE_IPS=($RESOLVED_IPS)
+if [ ${#NODE_IPS[@]} -eq 0 ]; then
+  echo "No IP addresses resolved for hnsdoh.com. Exiting."
+  exit 1
+fi
+
+
+# Define the domain and host for kdig commands
+TLS_HOST="hnsdoh.com"
+DOH_URL="https://hnsdoh.com/dns-query"
+
+
+while true; do
+
+# Loop over each IP and run the kdig commands
+for NODE_IP in "${NODE_IPS[@]}"
+do
+    echo "Running kdig commands for NODE_IP=$NODE_IP"
+
+    # Run the kdig commands
+    kdig +tls +tls-host=$TLS_HOST @$NODE_IP 1.wdbrn TXT +short
+    kdig +tls-ca +https=@$DOH_URL @$NODE_IP 2.wdbrn TXT +short
+    kdig @$NODE_IP 3.wdbrn TXT +short
+
+    echo "Completed kdig commands for NODE_IP=$NODE_IP"
+    echo "--------------------------------------------"
+done
+
+done
